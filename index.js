b8d7c8500e0a254b4eb0cc6080aad5aee72d6f59
Mario Rieker <mario.rieker@hfg-gmuend.de>
2018-03-20 20:44:41 +0100

    updated index.js for express webserver

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("hello world");
+//console.log("hello world");
+
+var express = require('express')
+var app = express()
+
+app.get('/', function (req, res) {
+  res.send('ok')
+})
+
+app.listen(8080)
